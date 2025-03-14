9f1431894b0ec3c033b401cedd90e86cd89208ce
Marin <marin.cadro@gmail.com>
2025-03-14 14:20:52 +0100

    Classe métier

--- src/metier/Region.java
+++ src/metier/Region.java
@@ -1,5 +1,36 @@
 package metier;
 
 public class Region {
-	String D;
+	int idRegion;
+	String LibelleRegion;
+	
+	public Region(int idRegion, String libelleRegion) {
+		super();
+		this.idRegion = idRegion;
+		LibelleRegion = libelleRegion;
+	}
+
+	public int getIdRegion() {
+		return idRegion;
+	}
+
+	public void setIdRegion(int idRegion) {
+		this.idRegion = idRegion;
+	}
+
+	public String getLibelleRegion() {
+		return LibelleRegion;
+	}
+
+	public void setLibelleRegion(String libelleRegion) {
+		LibelleRegion = libelleRegion;
+	}
+
+	@Override
+	public String toString() {
+		return "Region [idRegion=" + idRegion + ", LibelleRegion=" + LibelleRegion + "]";
+	}
+	
+	
+	
 }
--- src/metier/Utilisateur.java
+++ src/metier/Utilisateur.java
@@ -1,10 +1,155 @@
 package metier;
 
+import java.util.Date;
+import java.util.List;
+
+
 public class Utilisateur {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	String nom;
+	String prenom;
+	String login;
+	String adresse;
+	String cp;
+	String ville;
+	Date dateEmbauche;
+	String email;
+	String telfixe;
+	String telPortable;
+	Region region;
+	Role role;
+	private List<FicheFrais> lesFicheFrais;
+	
+	public Utilisateur(String nom, String prenom, String login, String adresse, String cp, String ville,
+			Date dateEmbauche, String email, String telfixe, String telPortable, Region region, Role role,
+			List<FicheFrais> lesFicheFrais) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.login = login;
+		this.adresse = adresse;
+		this.cp = cp;
+		this.ville = ville;
+		this.dateEmbauche = dateEmbauche;
+		this.email = email;
+		this.telfixe = telfixe;
+		this.telPortable = telPortable;
+		this.region = region;
+		this.role = role;
+		this.lesFicheFrais = lesFicheFrais;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getCp() {
+		return cp;
+	}
+
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public Date getDateEmbauche() {
+		return dateEmbauche;
+	}
+
+	public void setDateEmbauche(Date dateEmbauche) {
+		this.dateEmbauche = dateEmbauche;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelfixe() {
+		return telfixe;
+	}
+
+	public void setTelfixe(String telfixe) {
+		this.telfixe = telfixe;
+	}
+
+	public String getTelPortable() {
+		return telPortable;
+	}
+
+	public void setTelPortable(String telPortable) {
+		this.telPortable = telPortable;
+	}
+
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public List<FicheFrais> getLesFicheFrais() {
+		return lesFicheFrais;
+	}
+
+	public void setLesFicheFrais(List<FicheFrais> lesFicheFrais) {
+		this.lesFicheFrais = lesFicheFrais;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [nom=" + nom + ", prenom=" + prenom + ", login=" + login + ", adresse=" + adresse + ", cp="
+				+ cp + ", ville=" + ville + ", dateEmbauche=" + dateEmbauche + ", email=" + email + ", telfixe="
+				+ telfixe + ", telPortable=" + telPortable + ", region=" + region + ", role=" + role
+				+ ", lesFicheFrais=" + lesFicheFrais + "]";
+	}
+	
 }
+	
+	
