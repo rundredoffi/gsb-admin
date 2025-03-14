0548c49e4cc9941f0b2631edc6857f7e2ef45c3b
servaispic-m <servaispic-m@saint-louis29.net>
2025-03-14 14:45:04 +0100

    test accesdata

---
+++ src/persistance/AccesData.java
@@ -0,0 +1,16 @@
+package persistance;
+
+import java.util.List;
+
+import metier.Utilisateur;
+
+public class AccesData {
+
+	// création d’une session hibernate 
+	// public static Session s = HibernateSession.getSession();
+	
+	public static Utilisateur getUtlisateur() {
+		return null;
+	}
+
+}
