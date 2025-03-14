95ee91bdcc1a94ed8ccb5b258bc9431fc2f12db0
Marin <marin.cadro@gmail.com>
2025-03-14 15:39:04 +0100

    :construction: TestProgFin

--- src/vue/progprin.java
+++ src/vue/progprin.java
@@ -1,10 +1,16 @@
 package vue;
 import persistance.HibernateSession;
+
+import java.util.List;
+
+import metier.Utilisateur;
 import persistance.AccesData;
 
 public class progprin {
 	public static void main(String[] args) {
-		
+		List<Utilisateur> unUtilisateur = AccesData.getLesUtilisateur();
+		System.out.println(unUtilisateur.toString());
+		System.out.println("******************************");
 
 	}
 
