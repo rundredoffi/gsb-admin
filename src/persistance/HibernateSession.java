6d7d06de03c27ef0aa489a36bddd8359681c2a3c
Marin <marin.cadro@gmail.com>
2025-03-14 14:55:07 +0100

    :wrench: HibernateSession

---
+++ src/persistance/HibernateSession.java
@@ -0,0 +1,34 @@
+package persistance;
+
+import java.util.TimeZone;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSession {
+	
+	 private static final SessionFactory sessionFactory;
+	    static {
+	        try {
+	            // Build a SessionFactory object from session-factory config
+	            // defined in the hibernate.cfg.xml file. In this file we
+	            // register the JDBC connection information, connection pool,
+	            // the hibernate dialect that we used and the mapping to our
+	            // hbm.xml file for each pojo (plain old java object).
+	            Configuration config = new Configuration();
+	            sessionFactory = config.configure().buildSessionFactory();
+	        } catch (Throwable e) {
+	            System.err.println("Error in creating SessionFactory object."
+	                    + e.getMessage());
+	            throw new ExceptionInInitializerError(e);
+	        }
+	    }
+
+	// Renvoie une session Hibernate
+	public static Session getSession() throws HibernateException {
+		 sessionFactory.withOptions().jdbcTimeZone(TimeZone.getTimeZone("UTC"));
+		return sessionFactory.openSession();
+	}
+}
