e430e3bacd458b05df2695c1dd9f2d403917d36f
Rundredoffi <contact.rundred@gmail.com>
2025-03-21 13:55:02 +0100

    :construction: Getters/Setters utilisateurs

--- src/metier/Utilisateur.java
+++ src/metier/Utilisateur.java
@@ -96,4 +96,124 @@ public class Utilisateur {
                 + telfixe + ", telPortable=" + telPortable + ", region=" + region + ", role=" + role
                 + ", lesFicheFrais=" + lesFicheFrais + "]";
     }
+
+	public String getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+	public void setIdUtilisateur(String idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getCp() {
+		return cp;
+	}
+
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public Date getDateEmbauche() {
+		return dateEmbauche;
+	}
+
+	public void setDateEmbauche(Date dateEmbauche) {
+		this.dateEmbauche = dateEmbauche;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelfixe() {
+		return telfixe;
+	}
+
+	public void setTelfixe(String telfixe) {
+		this.telfixe = telfixe;
+	}
+
+	public String getTelPortable() {
+		return telPortable;
+	}
+
+	public void setTelPortable(String telPortable) {
+		this.telPortable = telPortable;
+	}
+
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public List<FicheFrais> getLesFicheFrais() {
+		return lesFicheFrais;
+	}
+
+	public void setLesFicheFrais(List<FicheFrais> lesFicheFrais) {
+		this.lesFicheFrais = lesFicheFrais;
+	}
 }
