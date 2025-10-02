3e9a589469b13e5378e3f4a61331dcc8464e464b
Rundredoffi <contact.rundred@gmail.com>
2025-10-02 22:58:54 +0200

    :recycle: Refacto du code

--- src/vue/Logout.java
+++ src/vue/Logout.java
@@ -2,13 +2,19 @@ package vue;
 
 import org.hibernate.Session;
 import persistance.HibernateSession;
+import utils.Logger;
 
 import javax.swing.*;
 
 public class Logout {
+    
+    private Logout() {
+        // Constructeur privé pour empêcher l'instanciation
+    }
+    
     public static void logout(JFrame currentFrame) {
         // Perform logout operations here, e.g., clearing session, etc.
-        System.out.println("User logged out");
+        Logger.info("User logged out");
 
         // Close the Hibernate session
         Session session = HibernateSession.getSession();
@@ -18,12 +24,10 @@ public class Logout {
         currentFrame.dispose();
 
         // Redirect to login view
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Login loginScreen = new Login();
-                loginScreen.addLoginListener(new Menu());
-                loginScreen.setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            Login loginScreen = new Login();
+            loginScreen.addLoginListener(new Menu());
+            loginScreen.setVisible(true);
         });
     }
 }
