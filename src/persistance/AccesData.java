35f0874135447597a74bd8880ca732296d7a7254
Rundredoffi <contact.rundred@gmail.com>
2025-10-02 21:44:09 +0200

    :recycle: Refacto du code pour faciliter la compréhension

--- src/persistance/AccesData.java
+++ src/persistance/AccesData.java
@@ -28,6 +28,12 @@ public class AccesData {
 		throw new IllegalStateException("Utility class");
 	}
 
+	// Constantes pour les paramètres des procédures stockées
+	private static final String PARAM_MOIS = "mois";
+	private static final String PARAM_REGION = "region";
+	private static final String PARAM_P_MOIS = "p_mois";
+	private static final String PARAM_P_REGION = "p_region";
+
 	private static Session s = HibernateSession.getSession();
 
 	public static List<Utilisateur> getLesUtilisateur() {
@@ -174,30 +180,28 @@ public class AccesData {
 	
 	public static List<Object[]> getStatsFFMontRegion(String month, int region) {
         StoredProcedureQuery query = s.createStoredProcedureQuery("get_stats_ffmont_region");
-        query.registerStoredProcedureParameter("mois", String.class, javax.persistence.ParameterMode.IN);
-        query.registerStoredProcedureParameter("region", Integer.class, javax.persistence.ParameterMode.IN);
-        query.setParameter("mois", month);
-        query.setParameter("region", region);
+        query.registerStoredProcedureParameter(PARAM_MOIS, String.class, javax.persistence.ParameterMode.IN);
+        query.registerStoredProcedureParameter(PARAM_REGION, Integer.class, javax.persistence.ParameterMode.IN);
+        query.setParameter(PARAM_MOIS, month);
+        query.setParameter(PARAM_REGION, region);
         query.execute();
         return query.getResultList();
     }
 
-    public static List<Object[]> getStatsHFMontRegion(String month, int region) {
+	public static List<Object[]> getStatsHFMontRegion(String month, int region) {
         StoredProcedureQuery query = s.createStoredProcedureQuery("get_stats_hfmont_region");
-        query.registerStoredProcedureParameter("mois", String.class, javax.persistence.ParameterMode.IN);
-        query.registerStoredProcedureParameter("region", Integer.class, javax.persistence.ParameterMode.IN);
-        query.setParameter("mois", month);
-        query.setParameter("region", region);
+        query.registerStoredProcedureParameter(PARAM_MOIS, String.class, javax.persistence.ParameterMode.IN);
+        query.registerStoredProcedureParameter(PARAM_REGION, Integer.class, javax.persistence.ParameterMode.IN);
+        query.setParameter(PARAM_MOIS, month);
+        query.setParameter(PARAM_REGION, region);
         query.execute();
         return query.getResultList();
-    }
-
-    public static List<Object[]> getStatsHfRegion(String month, int region) {
+    }    public static List<Object[]> getStatsHfRegion(String month, int region) {
         StoredProcedureQuery query = s.createStoredProcedureQuery("get_stats_hf_region");
-        query.registerStoredProcedureParameter("mois", String.class, javax.persistence.ParameterMode.IN);
-        query.registerStoredProcedureParameter("region", Integer.class, javax.persistence.ParameterMode.IN);
-        query.setParameter("mois", month);
-        query.setParameter("region", region);
+        query.registerStoredProcedureParameter(PARAM_MOIS, String.class, javax.persistence.ParameterMode.IN);
+        query.registerStoredProcedureParameter(PARAM_REGION, Integer.class, javax.persistence.ParameterMode.IN);
+        query.setParameter(PARAM_MOIS, month);
+        query.setParameter(PARAM_REGION, region);
         query.execute();
         return query.getResultList();
     }
@@ -207,35 +211,11 @@ public class AccesData {
         List<Object[]> statsHF = getStatsHFMontRegion(month, region);
         List<Object[]> statsHfRegion = getStatsHfRegion(month, region);
 
-       
-
-        // Combine the results into a single list
         List<Object[]> combinedStats = new ArrayList<>();
         for (Object[] ff : statsFF) {
             String idUtilisateur = (String) ff[0];
-            boolean foundHF = false;
-            boolean foundHfRegion = false;
-            double montantFraisHorsForfait = 0;
-            int nbFraisHorsForfait = 0;
-
-            for (Object[] hf : statsHF) {
-                if (hf[0].equals(idUtilisateur)) {
-                    if (hf.length > 3) {
-                        montantFraisHorsForfait = ((Number) hf[3]).doubleValue();
-                    }
-                    foundHF = true;
-                    break;
-                }
-            }
-            for (Object[] hr : statsHfRegion) {
-                if (hr[0].equals(idUtilisateur)) {
-                    if (hr.length > 3) {
-                        nbFraisHorsForfait = ((Number) hr[3]).intValue();
-                    }
-                    foundHfRegion = true;
-                    break;
-                }
-            }
+            double montantFraisHorsForfait = findMontantFraisHorsForfait(statsHF, idUtilisateur);
+            int nbFraisHorsForfait = findNbFraisHorsForfait(statsHfRegion, idUtilisateur);
 
             Object[] combined = Arrays.copyOf(ff, ff.length + 2);
             combined[4] = montantFraisHorsForfait;
@@ -244,6 +224,24 @@ public class AccesData {
         }
 
         return combinedStats;
+    }
+
+    private static double findMontantFraisHorsForfait(List<Object[]> statsHF, String idUtilisateur) {
+        for (Object[] hf : statsHF) {
+            if (hf[0].equals(idUtilisateur) && hf.length > 3) {
+                return ((Number) hf[3]).doubleValue();
+            }
+        }
+        return 0;
+    }
+
+    private static int findNbFraisHorsForfait(List<Object[]> statsHfRegion, String idUtilisateur) {
+        for (Object[] hr : statsHfRegion) {
+            if (hr[0].equals(idUtilisateur) && hr.length > 3) {
+                return ((Number) hr[3]).intValue();
+            }
+        }
+        return 0;
     }
     
     public static List<FicheFrais> retrieveFicheFraisByRegion(int regionId) {
@@ -255,10 +253,10 @@ public class AccesData {
     
     public static Object getMoyenneMontantFraisForfait(String month, int region) {
         StoredProcedureQuery query = s.createStoredProcedureQuery("GetMoyenneMontantFraisForfait")
-            .registerStoredProcedureParameter("p_mois", String.class, javax.persistence.ParameterMode.IN)
-            .registerStoredProcedureParameter("p_region", Integer.class, javax.persistence.ParameterMode.IN)
-            .setParameter("p_mois", month)
-            .setParameter("p_region", region);
+            .registerStoredProcedureParameter(PARAM_P_MOIS, String.class, javax.persistence.ParameterMode.IN)
+            .registerStoredProcedureParameter(PARAM_P_REGION, Integer.class, javax.persistence.ParameterMode.IN)
+            .setParameter(PARAM_P_MOIS, month)
+            .setParameter(PARAM_P_REGION, region);
 
         Logger.debug("Appel de la procédure avec mois : " + month + ", région : " + region);
 
@@ -275,10 +273,10 @@ public class AccesData {
 
     public static Object getMoyenneMontantFraisHorsForfait(String month, int region) {
         StoredProcedureQuery query = s.createStoredProcedureQuery("GetMoyenneMontantFraisHorsForfait")
-                .registerStoredProcedureParameter("p_mois", String.class, javax.persistence.ParameterMode.IN)
-                .registerStoredProcedureParameter("p_region", Integer.class, javax.persistence.ParameterMode.IN)
-                .setParameter("p_mois", month)
-                .setParameter("p_region", region);
+                .registerStoredProcedureParameter(PARAM_P_MOIS, String.class, javax.persistence.ParameterMode.IN)
+                .registerStoredProcedureParameter(PARAM_P_REGION, Integer.class, javax.persistence.ParameterMode.IN)
+                .setParameter(PARAM_P_MOIS, month)
+                .setParameter(PARAM_P_REGION, region);
 
         // Exécuter la requête et obtenir une liste de résultats
         query.execute();
