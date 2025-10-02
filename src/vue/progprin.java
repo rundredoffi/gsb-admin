6ec8c3d4768d523aee409dd3c3e4b6f1fb356686
Rundredoffi <contact.rundred@gmail.com>
2025-10-02 23:07:54 +0200

    :recycle: Refacto

--- src/vue/progprin.java
+++ src/vue/progprin.java
@@ -1,21 +1,13 @@
 package vue;
-import persistance.HibernateSession;
+import utils.Logger;
 
-import javax.swing.JFrame;
-
-import listeners.LoginListener;
-import metier.Utilisateur;
-
-public class progprin {
+public class ProgPrin {
     public static void main(String[] args) {
         Login loginScreen = new Login();
-        loginScreen.addLoginListener(new LoginListener() {
-            @Override
-            public void onLoginSuccess(Utilisateur util, JFrame loginFrame) {
-                System.out.println("Utilisateur connecté : " + util.getPrenom() + " " + util.getNom());
-                loginFrame.dispose(); // Fermer la fenêtre de login
-                new Menu(util);
-            }
+        loginScreen.addLoginListener((util, loginFrame) -> {
+            Logger.info("Utilisateur connecté : " + util.getPrenom() + " " + util.getNom());
+            loginFrame.dispose(); // Fermer la fenêtre de login
+            new Menu(util);
         });
 
         loginScreen.setVisible(true);
